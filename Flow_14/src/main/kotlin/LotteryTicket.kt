9cb5655b4b41d4bb1b8380e76160e4927543303b
Ekaterina <iea7979@mail.ru>
2023-01-21 12:00:05 +0300

    name of number list changed

--- Flow_14/src/main/kotlin/LotteryTicket.kt
+++ Flow_14/src/main/kotlin/LotteryTicket.kt
@@ -6,16 +6,16 @@ class LotteryTicket {
     private val lotteryTicket = Array(3) { Array(9) { 0 } }
 
     fun getLotteryTicket(): Array<Array<Int>> {
-        val numbersList: MutableList<Int> = (1..90).toMutableList()
-        shuffle(numbersList)
+        val numbersList1: MutableList<Int> = (1..90).toMutableList()
+        shuffle(numbersList1)
         val listNull = listOf(0, 0, 0, 0)
-        val row1 = numbersList.subList(0, 5)
+        val row1 = numbersList1.subList(0, 5)
         val rowList1 = row1 + listNull
         shuffle(rowList1)
-        val row2 = numbersList.subList(6, 11)
+        val row2 = numbersList1.subList(6, 11)
         val rowList2 = row2 + listNull
         shuffle(rowList2)
-        val row3 = numbersList.subList(12, 17)
+        val row3 = numbersList1.subList(12, 17)
         val rowList3 = row3 + listNull
         shuffle(rowList3)
         for (i in lotteryTicket.indices) {
